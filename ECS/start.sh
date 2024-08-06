a69b9aac7eafc121654ce50ef99df63ed65e0a21
Dinush Chathurya <dinushchathurya21@gmail.com>
2024-08-06 11:56:25 +0100

    feat: Create start.sh

--- ECS/start.sh
+++ ECS/start.sh
@@ -1,46 +1,34 @@
 #!/bin/bash
+set -euo pipefail
 
-# Exit immediately if a command exits with a non-zero status
-set -e
+check_env() {
+    if [ -z "${GITHUB_PAT:-}" ]; then
+        echo "Env variable GITHUB_PAT is required but not set"
+        exit 1
+    fi
 
-# Ensure the required environment variables are set
-if [ -z "$GITHUB_TOKEN" ]; then
-  echo "GITHUB_TOKEN is not set. Please set it to a GitHub personal access token."
-  exit 1
-fi
+    if [ -z "${GITHUB_OWNER:-}" ]; then
+        echo "Env variable GITHUB_OWNER is required but not set"
+        exit 1
+    fi
 
-if [ -z "$RUNNER_NAME" ]; then
-  echo "RUNNER_NAME is not set. Please set it to a name for this runner."
-  exit 1
-fi
+    if [ -z "${GITHUB_REPO:-}" ]; then
+        echo "Env variable GITHUB_REPO is required but not set"
+        exit 1
+    fi
+}
 
-if [ -z "$RUNNER_SCOPE" ]; then
-  echo "RUNNER_SCOPE is not set. Please set it to either 'user' or 'repository'."
-  exit 1
-fi
+register_runner() {
+    local github_token=$(curl -sL \
+        -X POST \
+        -H "Accept: application/vnd.github+json" \
+        -H "Authorization: Bearer $GITHUB_PAT" \
+        -H "X-GitHub-Api-Version: 2022-11-28" \
+        https://api.github.com/repos/$GITHUB_OWNER/$GITHUB_REPO/actions/runners/registration-token | jq -r .token)
 
-if [ -z "$GITHUB_OWNER" ]; then
-  echo "GITHUB_OWNER is not set. Please set it to the owner of the repository or your GitHub username."
-  exit 1
-fi
+    ./config.sh --unattended --url https://github.com/$GITHUB_OWNER/$GITHUB_REPO --token $github_token
+}
 
-if [ "$RUNNER_SCOPE" = "repository" ] && [ -z "$GITHUB_REPOSITORY" ]; then
-  echo "GITHUB_REPOSITORY is not set. Please set it to the repository name in 'owner/repo' format."
-  exit 1
-fi
-
-# Determine the URL based on the scope
-if [ "$RUNNER_SCOPE" = "user" ]; then
-  RUNNER_URL="https://github.com/$GITHUB_OWNER"
-elif [ "$RUNNER_SCOPE" = "repository" ]; then
-  RUNNER_URL="https://github.com/$GITHUB_REPOSITORY"
-else
-  echo "Invalid RUNNER_SCOPE value. It should be either 'user' or 'repository'."
-  exit 1
-fi
-
-# Configure the GitHub Actions runner
-./config.sh --url "${RUNNER_URL}" --token "${GITHUB_TOKEN}" --name "${RUNNER_NAME}" --work "_work" --replace
-
-# Run the GitHub Actions runner
+check_env
+register_runner
 ./run.sh
